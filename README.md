8d3eb082a56bab1ba43a3d3ea2401f8392c52ad2
Devlin Brush <devlinbrush@gmail.com>
2021-04-13 23:16:27 -0500

    started building more of player selector interface

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Fantasy Basketball Assistant
 ### API Information
-I am using the [balldontlie API](https://www.balldontlie.io/#introduction) for player stats and NBA schedule data.
+I am using the NBA Data API for player stats and NBA schedule data. Documentation on how to use this API can be found [here](https://github.com/kashav/nba.js/blob/master/docs/api/DATA.md)
--- static/playersearch.js
+++ static/playersearch.js
@@ -1,12 +1,39 @@
 const $searchBar = $('#player-search-bar');
 const $searchResults = $('#player-search-results');
+const $playerList = $('#player-list')
 const $playerChoices = $('select[name="players"]').children('option');
-const names = getPlayerNames($playerChoices)
+const names = getPlayerNames($playerChoices);
 
+//TODO: Need objects or something to track both names and IDs. Maybe this just collects the option objects and I derive names and IDs later on?
 function getPlayerNames($choices) {
     return $choices.map(function(){ return this.innerText }).get()
 }
 
-function showResults() {
-
+function showResultsOnFocus() {
+    $playerList.append('<p class="lead">Choose players here</p>');
+    $searchResults.attr('hidden', false);
 }
+
+function hideResultsOnFocusOut() {
+    $playerList.empty();
+    $searchResults.attr('hidden', true);
+}
+
+function suggestPlayerName(evt) {
+    const term = $(evt.target).value();
+    matched = names.map(name => {
+        const regex = new Regex(term)
+        return regex.test(name)
+    });
+    populatePlayerNames(matched);
+}
+
+//TODO: This should not just handle names - ID or data needs to contain ID number
+function populatePlayerNames(list) {
+    for (let player of list) {
+        const li = $(`<li class="player-choice">`)
+    }
+}
+
+$searchBar.focusin(showResultsOnFocus);
+$searchBar.focusout(hideResultsOnFocusOut);
