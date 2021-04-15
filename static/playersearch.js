00d28afd2003bd9efe0b8168935353e8cf736636
Devlin Brush <devlinbrush@gmail.com>
2021-04-15 18:15:41 -0500

    player search feature implemented for team builder

--- static/playersearch.js
+++ static/playersearch.js
@@ -2,38 +2,82 @@ const $searchBar = $('#player-search-bar');
 const $searchResults = $('#player-search-results');
 const $playerList = $('#player-list')
 const $playerChoices = $('select[name="players"]').children('option');
-const names = getPlayerNames($playerChoices);
+const players = getPlayers($playerChoices);
 
-//TODO: Need objects or something to track both names and IDs. Maybe this just collects the option objects and I derive names and IDs later on?
-function getPlayerNames($choices) {
-    return $choices.map(function(){ return this.innerText }).get()
+/**
+ * Takes the player choice options from the form and returns an array with
+ * the IDs and names associated.
+ * @param {*} $choices = jQuery collection of option elements representing player choices from the form
+ * @returns Array
+ */
+function getPlayers($choices) {
+    return $choices.map(function(){ 
+        return {name: this.innerText, ID: this.value} 
+    }).get()
 }
 
-function showResultsOnFocus() {
-    $playerList.append('<p class="lead">Choose players here</p>');
-    $searchResults.attr('hidden', false);
-}
-
-function hideResultsOnFocusOut() {
+function showEmptyMessage() {
     $playerList.empty();
-    $searchResults.attr('hidden', true);
+    $playerList.append('<p class="lead">Choose players here</p>');
 }
 
-function suggestPlayerName(evt) {
-    const term = $(evt.target).value();
-    matched = names.map(name => {
-        const regex = new Regex(term)
-        return regex.test(name)
-    });
-    populatePlayerNames(matched);
-}
-
-//TODO: This should not just handle names - ID or data needs to contain ID number
-function populatePlayerNames(list) {
-    for (let player of list) {
-        const li = $(`<li class="player-choice">`)
+function showResultsOnFocus(evt) {
+    $searchResults.attr('hidden', false);
+    //If the search bar is empty, append a message. Otherwise, continue to show the players matching the current search.
+    if (!evt.target.value) {
+        showEmptyMessage();
     }
 }
 
-$searchBar.focusin(showResultsOnFocus);
-$searchBar.focusout(hideResultsOnFocusOut);
+function hideResultsOnClick(evt) {
+    const container = $("#player-search");
+    // if the target of the click isn't the container of the search nor a descendant of the container, hide the search results
+    if (!container.is(evt.target) && container.has(evt.target).length === 0) 
+    {
+        hideResults();
+    }
+}
+
+function hideResults() {
+    $searchResults.attr('hidden', true);
+}
+
+function handleSearch(evt) {
+    const term = $(evt.target).val();
+    if (!term) {
+        showEmptyMessage();
+    } else {
+        matches = getMatchedPlayers(term);
+        populateMatchedPlayerNames(matches);
+    }
+}
+
+function getMatchedPlayers(term) {
+    return players.filter(player => matchPlayerName(term, player))
+}
+
+function matchPlayerName(term, player) {
+    const regex = new RegExp(term, "i")
+    return regex.test(player.name)
+}
+
+function populateMatchedPlayerNames(matches) {
+    $playerList.empty();
+    for (let player of matches) {
+        const li = $(`<li class="searchplayer-choice" id="${player.ID}">${player.name}</li>`)
+        $playerList.append(li)
+    }
+}
+
+function handlePlayerChoice(evt) {
+    const choice = evt.target.id;
+    $(`option[value="${choice}"]`).prop('selected', true)
+    $searchBar.val('')
+    $playerList.empty();
+    hideResults();
+}
+
+$searchBar.on('focusin', showResultsOnFocus);
+$searchBar.keyup(handleSearch);
+$playerList.on('click', 'li', handlePlayerChoice);
+$(document).on('mouseup', hideResultsOnClick);
